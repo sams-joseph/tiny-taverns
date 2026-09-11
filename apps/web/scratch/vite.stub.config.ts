c3ca77ce4590728949f02ff0a683e55ebc86b453
Joseph Sams <samsjb87@hotmail.com>
2026-09-11 09:20:11 -0400

    Cleanup

--- apps/web/scratch/vite.stub.config.ts
+++
@@ -1,5 +0,0 @@
-import { mergeConfig } from "vite";
-import base from "../vite.config";
-export default mergeConfig(base, {
-  resolve: { alias: [{ find: /^vitest$/, replacement: new URL("./vitest-shim.ts", import.meta.url).pathname }] },
-});
